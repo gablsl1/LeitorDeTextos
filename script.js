727aa416093cdcf47ad8193aef7f75b903a5594e
gblsl1 <gabrielsousadeveloper@gmail.com>
2022-12-27 18:03:12 -0300

    adicionando botão e ação de pausa

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const textarea = document.getElementById('texto')
-const button = document.getElementById('botao')
+const falar = document.getElementById('botao-falar')
+const pausar = document.getElementById('botao-pausar')
+const cancelar = document.getElementById('botao-cancelar')
 
-button.addEventListener('click', () => {
+falar.addEventListener('click', () => {
   const texto = textarea.value
   const speaks = new SpeechSynthesisUtterance(texto)
   speaks.lang = 'pt-BR'
   
   window.speechSynthesis.speak(speaks)
+})
+
+pausar.addEventListener('click', () => {
+  const synth = window.speechSynthesis
+  synth.pause()
 })
