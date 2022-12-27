12048b6799bc815fc8c52dacf20df7ec7db6f722
gblsl1 <gabrielsousadeveloper@gmail.com>
2022-12-27 18:14:09 -0300

    adicionando o botão e ação de despausar

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const textarea = document.getElementById('texto')
 const falar = document.getElementById('botao-falar')
 const pausar = document.getElementById('botao-pausar')
-const cancelar = document.getElementById('botao-cancelar')
+const despausar = document.getElementById('botao-despausar')
+const synth = window.speechSynthesis
 
 falar.addEventListener('click', () => {
   const texto = textarea.value
@@ -12,6 +13,9 @@ falar.addEventListener('click', () => {
 })
 
 pausar.addEventListener('click', () => {
-  const synth = window.speechSynthesis
   synth.pause()
+})
+
+despausar.addEventListener('click', () => {
+  synth.resume()
 })
